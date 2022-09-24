90071c9b71f5316c43a5ae22c4d0dbdff9c42b3b
adityakrisna826gmailcom <112675531+adityakrisna826gmailcom@users.noreply.github.com>
2022-09-24 13:37:35 +0700

    commit

--- AKP/src/main/java/ujian/ujiankelima/testng/ParseStringToNumeric.java
+++ AKP/src/main/java/ujian/ujiankelima/testng/ParseStringToNumeric.java
@@ -2,4 +2,233 @@ package ujian.ujiankelima.testng;
 
 public class ParseStringToNumeric {
 
+	public boolean parseToIntActual (String strInput)
+	{
+		boolean isEror  = true;
+		int intInput = 0;
+		try {
+			intInput = Integer.valueOf(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + intInput);
+		System.out.println("Ini Adalah Hasil Parsing To Int Actual: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	
+	public boolean parseToLongActual(String strInput)
+	{
+		boolean isEror  = true;
+		long longInput = 0;
+		try {
+			longInput = Long.valueOf(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + longInput);
+		System.out.println("Ini Adalah Hasil Parsing To Long Actual: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	public boolean parseToByteActual(String strInput)
+	{
+		boolean isEror  = true;
+		byte byteInput = 0;
+		try {
+			byteInput = Byte.valueOf(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + byteInput);
+		System.out.println("Ini Adalah Hasil Parsing To Byte Actual: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	public boolean parseToShortActual(String strInput)
+	{
+		boolean isEror  = true;
+		short shortInput = 0;
+		try {
+			shortInput = Short.valueOf(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + shortInput);
+		System.out.println("Ini Adalah Hasil Parsing To Short Actual: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	public boolean parseToFloatActual(String strInput)
+	{
+		boolean isEror  = true;
+		float floatInput = (float) 0.0;
+		try {
+			floatInput = Float.valueOf(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + floatInput);
+		System.out.println("Ini Adalah Hasil Parsing To Float Actual: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	public boolean parseToDoubleActual(String strInput)
+	{
+		boolean isEror  = true;
+		double douInput = 0.0;
+		try {
+			douInput = Long.valueOf(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + douInput);
+		System.out.println("Ini Adalah Hasil Parsing To Double Actual: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	
+	public boolean parseToIntExpect(String strInput)
+	{
+		boolean isEror  = true;
+		int intInput = 0;
+		try {
+			intInput = Integer.valueOf(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + intInput);
+		System.out.println("Ini Adalah Hasil Parsing To Int Expect: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	public boolean parseToLongExpect(String strInput)
+	{
+		boolean isEror  = true;
+		long longInput = 0;
+		try {
+			longInput = Long.valueOf(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + longInput);
+		System.out.println("Ini Adalah Hasil Parsing To Long Expect: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	public boolean parseToByteExpect(String strInput)
+	{
+		boolean isEror  = true;
+		byte byteInput = 0;
+		try {
+			byteInput = Byte.parseByte(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + byteInput);
+		System.out.println("Ini Adalah Hasil Parsing To Byte Expect: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	public boolean parseToShortExpect(String strInput)
+	{
+		boolean isEror  = true;
+		short shortInput = 0;
+		try {
+			shortInput = Short.parseShort(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + shortInput);
+		System.out.println("Ini Adalah Hasil Parsing To Short Expect: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	public boolean parseToFloatExpect(String strInput)
+	{
+		boolean isEror  = true;
+		float floatInput = (float) 0.0;
+		try {
+			floatInput = Float.parseFloat(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + floatInput);
+		System.out.println("Ini Adalah Hasil Parsing To Float Expect: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
+	
+	public boolean parseToDoubleExpect(String strInput)
+	{
+		boolean isEror  = true;
+		double douInput = 0.0;
+		try {
+			douInput = Double.parseDouble(strInput);
+			
+		}catch (Exception e)
+		{
+			System.out.println(e);
+			isEror = false;
+		}
+		
+		System.out.println("Ini Adalah Nilai Input Setelah Di Parsing : " + douInput);
+		System.out.println("Ini Adalah Hasil Parsing To Double Expect: " + isEror);
+		System.out.println("===========================================================================");
+		return isEror;
+	}
 }
--- AKP/src/test/java/ujian/ujiankelima/testng/TestParseStringToNumeric.java
+++ AKP/src/test/java/ujian/ujiankelima/testng/TestParseStringToNumeric.java
@@ -1,5 +1,141 @@
 package ujian.ujiankelima.testng;
 
+import java.util.Random;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+//import ujian.ujiankedua.ParseToNumeric;
+
 public class TestParseStringToNumeric {
 
+//	Scanner scn = new Scanner(System.in);
+	String strInput = "";
+	boolean bouCharacter = false;
+    boolean bouNumber = true;
+	int intLengthString =1;
+	int loopTest = 1;
+	int loopBefore = 1;
+	int loopAfter = 1;
+	ParseStringToNumeric a;
+	Random rand;
+	
+	@BeforeTest
+	   public void beforeTest() {
+		System.out.println("Test Dimulai !!");
+		a = new ParseStringToNumeric();
+		System.out.println("===========================================================================");
+	   }
+	
+	@BeforeClass
+	   public void beforeClass() {
+		System.out.println("Ini Adalah Before Class Test Parse To Numeric");
+		System.out.println("===========================================================================");
+	   }
+	
+	@BeforeMethod
+	   public void beforeMethod() {
+		System.out.println("Ini Adalah Before Method Test Parse To Numeric Yang Ke : " + loopBefore);
+		System.out.println("===========================================================================");
+		loopBefore ++;	
+	   }
+	
+	@Test(priority = 0)
+	public void testParseToInt()
+	{
+		System.out.println();
+		System.out.println("<--------- TEST STRING TO INT--------->");
+		rand = new Random();
+		intLengthString = rand.nextInt(0, 10);
+		strInput = RandomStringUtils.random(intLengthString, bouCharacter, bouNumber);
+		System.out.println("String Input: "+strInput);
+		AssertJUnit.assertEquals(a.parseToIntActual(strInput), a.parseToIntExpect(strInput));
+	}
+	
+	@Test(priority = 1)
+	public void testParseToLong()
+	{
+		System.out.println();
+		System.out.println("<--------- TEST STRING TO Long--------->");
+		rand = new Random();
+		intLengthString = rand.nextInt(1, 10);
+		strInput = RandomStringUtils.random(intLengthString, bouCharacter, bouNumber);
+		System.out.println("String Input: "+strInput);
+		AssertJUnit.assertEquals(a.parseToLongActual(strInput), a.parseToLongExpect(strInput));
+	}
+	
+	@Test(priority = 2)
+	public void testParseToByte()
+	{
+		System.out.println();
+		System.out.println("<--------- TEST STRING TO Byte--------->");
+		rand = new Random();
+		intLengthString = rand.nextInt(1, 5);
+		strInput = RandomStringUtils.random(intLengthString, bouCharacter, bouNumber);
+		System.out.println("String Input: "+strInput);
+		AssertJUnit.assertEquals(a.parseToByteActual(strInput), a.parseToByteExpect(strInput));
+	}
+	
+	@Test(priority = 3)
+	public void testParseToShort()
+	{
+		System.out.println();
+		System.out.println("<--------- TEST STRING TO Short--------->");
+		rand = new Random();
+		intLengthString = rand.nextInt(1, 10);
+		strInput = RandomStringUtils.random(intLengthString, bouCharacter, bouNumber);
+		System.out.println("String Input: "+strInput);
+		AssertJUnit.assertEquals(a.parseToShortActual(strInput), a.parseToShortExpect(strInput));
+	}
+	
+	@Test(priority = 4)
+	public void testParseToFloat()
+	{
+		System.out.println();
+		System.out.println("<--------- TEST STRING TO Float--------->");
+		rand = new Random();
+		intLengthString = rand.nextInt(1, 10);
+		strInput = RandomStringUtils.random(intLengthString, bouCharacter, bouNumber);
+		System.out.println("String Input: "+strInput);
+		AssertJUnit.assertEquals(a.parseToFloatActual(strInput), a.parseToFloatExpect(strInput));
+	}
+	
+	@Test(priority = 5)
+	public void testParseToDouble()
+	{
+		System.out.println();
+		System.out.println("<--------- TEST STRING TO Double--------->");
+		rand = new Random();
+		intLengthString = rand.nextInt(1, 10);
+		strInput = RandomStringUtils.random(intLengthString, bouCharacter, bouNumber);
+		System.out.println("String Input: "+strInput);
+		AssertJUnit.assertEquals(a.parseToDoubleActual(strInput), a.parseToDoubleExpect(strInput));
+	}
+	
+	@AfterMethod
+	   public void afterMethod() {
+		 System.out.println("Ini Adalah After Method Test Parse To Numeric Yang Ke : " + loopAfter);
+		 System.out.println("===========================================================================");
+		 loopAfter ++;
+	   }
+	 
+	 @AfterClass
+	   public void afterClass() {
+		 System.out.println("Ini Adalah After Class Test Parse To Numeric");
+		 System.out.println("===========================================================================");
+	   }
+	 
+	 @AfterTest
+	   public void afterTest() {
+		 System.out.println("Test Sudah Selesai Dilakukan");
+		 System.out.println("===========================================================================");
+	   }
 }
